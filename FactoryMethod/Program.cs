aebc97ee07588dbb3113df281ec29ac597c4d708
Kanan172411 <kanangarayev02@gmail.com>
2022-10-13 14:22:48 +0400

    add factory method pattern

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.Title = "Factory Method";
+﻿using FactoryMethod;
+
+Console.Title = "Factory Method";
+
+List<DiscountFactory> factories = new List<DiscountFactory>
+{
+    new CodeDiscountFactory(Guid.NewGuid()),
+    new CountryDiscountFactory("AZE")
+};
+
+foreach (var factory in factories)
+{
+    DiscountService discountService = factory.CreateDiscountService();
+    Console.WriteLine($"Percentage: {discountService.DiscountPercentage} " + $" Came from: {discountService}");
+}
