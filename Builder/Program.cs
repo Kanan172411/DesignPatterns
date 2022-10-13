00ec524700007cc3bc05c8167a15192982d8c77b
Kanan172411 <kanangarayev02@gmail.com>
2022-10-13 16:52:21 +0400

    add builder pattern

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.Title = "Builder";
+﻿using BuilderPattern;
+
+Console.Title = "Builder";
+
+Garage garage = new Garage();
+
+MercedesBuilder mercedesBuilder = new MercedesBuilder();
+BMWBuilder bmwBuilder = new BMWBuilder();
+
+garage.Construct(mercedesBuilder);
+Console.WriteLine(mercedesBuilder.Car.ToString());
+garage.Show();
+
+garage.Construct(bmwBuilder);
+Console.WriteLine(bmwBuilder.Car.ToString());
+garage.Show();
+
+Console.ReadKey();
