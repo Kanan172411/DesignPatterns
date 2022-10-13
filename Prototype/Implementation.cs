e44eac1bdf788789624717495bd5908074943419
Kanan172411 <kanangarayev02@gmail.com>
2022-10-13 17:47:18 +0400

    add Prototype pattern

--- Prototype/Implementation.cs
+++ Prototype/Implementation.cs
@@ -1,4 +1,53 @@
-﻿namespace Prototype
+﻿using Newtonsoft.Json;
+namespace Prototype
 {
+    //Prototype
+    public abstract class Person
+    {
+        public abstract string Name { get; set; }
 
+        public abstract Person Clone(bool deepClone);
+    }
+
+    //ConcretePrototype
+    public class Manager : Person
+    {
+        public override string Name { get ; set ; }
+
+        public Manager(string name)
+        {
+            Name = name;
+        }
+
+        public override Person Clone(bool deepClone = false)
+        {
+            var objectAsJson = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<Manager>(objectAsJson);
+            return (Person)MemberwiseClone();   
+        }
+    }
+
+    //ConcretePrototype
+    public class Employee : Person
+    {
+        public Manager Manager { get; set; }
+
+        public override string Name { get; set ; }
+
+        public Employee(Manager manager, string name)
+        {
+            Manager = manager;
+            Name = name;
+        }
+
+        public override Person Clone(bool deepClone = false)
+        {
+            if (deepClone)
+            {
+                var objectAsJson = JsonConvert.SerializeObject(this);
+                return JsonConvert.DeserializeObject<Employee>(objectAsJson);
+            }
+            return(Employee)MemberwiseClone();
+        }
+    }
 }
--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.Title = "Prototype";
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+Manager manager = new Manager("Les");
+Manager managerClone = (Manager)manager.Clone();
+Console.WriteLine($"Manager has been cloned: {managerClone.Name}");
+
+Employee employee = new Employee(managerClone, "Jackson");
+Employee employeeClone = (Employee)employee.Clone(true);
+Console.WriteLine($"Employee has been cloned: {employeeClone.Name} " + $" with manager {employeeClone.Manager.Name}");
+
+managerClone.Name = "Kanan";
+Console.WriteLine($"Employee has been cloned: {employeeClone.Name} " + $" with manager {employeeClone.Manager.Name}");
