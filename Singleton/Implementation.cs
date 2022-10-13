8af089a7f1312cee053f8650daabd6ea3439098e
Kanan172411 <kanangarayev02@gmail.com>
2022-10-13 12:50:41 +0400

    add singleton pattern

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,4 +1,24 @@
 ﻿namespace Singleton
 {
-  
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _instanceLazy = new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _instanceLazy.Value;
+            }
+        }
+
+        protected Logger()
+        {
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
 }
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Title = "Singleton";
+﻿using Singleton;
+
+Console.Title = "Singleton";
+
+Logger instance1 = Logger.Instance;
+Logger instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance1 == Logger.Instance)
+{
+    Console.WriteLine("Instances are same");
+}
+
+instance1.Log($"Message from: { nameof(instance1)}");
+instance2.Log($"Message from: { nameof(instance2)}");
+
+Logger.Instance.Log($"Message from: {nameof(Logger.Instance)}");
