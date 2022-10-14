86976014ad912cd712ff16e7b1955b017a16cdd4
Kanan172411 <kanangarayev02@gmail.com>
2022-10-14 17:42:17 +0400

    add decorator pattern

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.Title = "Decorator";
+﻿using Decorator;
+
+Console.Title = "Decorator";
+
+var cloudMailService = new CloudMailService();
+cloudMailService.SendMail("Hello there!");
+
+var onPremiseMailService = new OnPremiseMailService();
+onPremiseMailService.SendMail("General Kenobi!");
+
+var statisticDecorator = new StatisticDecorator(cloudMailService);
+statisticDecorator.SendMail($"Hello there via {nameof(StatisticDecorator)} wrapper.");
+
+var messageDatabaseDecorator = new MessageDatabaseDecorator(onPremiseMailService);
+messageDatabaseDecorator.SendMail($"Hello there via {nameof(MessageDatabaseDecorator)} wrapper message 1");
+messageDatabaseDecorator.SendMail($"Hello there via {nameof(MessageDatabaseDecorator)} wrapper message 2");
+
+foreach (var item in messageDatabaseDecorator.SentMessages)
+{
+    Console.WriteLine($"Stored message: \"{item}\"");
+}
