bb39a6df1d567ef87d2eb2ef69131b62e9ce5cb6
Kanan172411 <kanangarayev02@gmail.com>
2022-10-14 18:07:58 +0400

    add facade pattern

--- Facade/Implementation.cs
+++ Facade/Implementation.cs
@@ -1,3 +1,54 @@
 ﻿namespace Facade
 {
+    //SubsytemClass
+    public class OrderService
+    {
+        public bool HasEnoughOrder(int customerId)
+        {
+            return (customerId > 5);
+        }
+    }
+
+    //SubsystemClass
+    public class CustomerDiscountBaseService
+    {
+        public double CalculateDiscountBase(int customerId)
+        {
+            return (customerId > 8) ? 10 : 20;
+        }
+    }
+
+    //SubsytemClass
+    public class DayOfTheWeekFactorService
+    {
+        public double CalculateDayOfTheWeekFactor()
+        {
+            switch (DateTime.UtcNow.DayOfWeek)
+            {
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    return 0.8;
+                default:
+                    return 1.2;
+            }
+        }
+    }
+
+    //Facade
+    public class DiscountFacade
+    {
+        private readonly OrderService _orderService = new();
+        private readonly CustomerDiscountBaseService _customerDiscountService = new();
+        private readonly DayOfTheWeekFactorService _dayOfTheWeekFactorService = new();
+
+        public double CalculateDiscountPercentage(int customerId)
+        {
+            if (!_orderService.HasEnoughOrder(customerId))
+            {
+                return 0;
+            }
+
+            return _customerDiscountService.CalculateDiscountBase(customerId) * _dayOfTheWeekFactorService.CalculateDayOfTheWeekFactor();
+        }
+    }
 }
--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.Title = "Facade";
+﻿using Facade;
+
+Console.Title = "Facade";
+
+var facade = new DiscountFacade();
+
+Console.WriteLine($"For id with 1: {facade.CalculateDiscountPercentage(1)}");
+Console.WriteLine($"For id with 10: {facade.CalculateDiscountPercentage(10)}");
