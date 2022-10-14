2c62ec43f26dd636edc4e68c3317be55864dcf14
Kanan172411 <kanangarayev02@gmail.com>
2022-10-14 16:51:32 +0400

    add bridge pattern

--- Bridge/Implementation.cs
+++ Bridge/Implementation.cs
@@ -1,3 +1,67 @@
 ﻿namespace Bridge
 {
+    //Abstraction
+    public abstract class Menu
+    {
+        public readonly ICoupon _coupon = null!;
+
+        public abstract int CalculatePrice();
+
+        public Menu(ICoupon coupon)
+        {
+            _coupon = coupon;
+        }
+    }
+
+    //RefinedAbstraction
+    public class VegettarianMenu : Menu
+    {
+        public VegettarianMenu(ICoupon coupon): base(coupon) 
+        {
+
+        }
+
+        public override int CalculatePrice()
+        {
+            return 20 - _coupon.CouponValue;
+        }
+    }
+
+    //RefinedAbstraction
+    public class MeatbasedMenu : Menu
+    {
+        public MeatbasedMenu(ICoupon coupon): base(coupon) 
+        {
+
+        }
+
+        public override int CalculatePrice()
+        {
+            return 30 - _coupon.CouponValue;
+        }
+    }
+
+    //Implementor
+    public interface  ICoupon
+    {
+        int CouponValue { get; }
+    }
+
+    //ConcreteImplementation
+    public class NoCoupon : ICoupon
+    {
+        public int CouponValue { get => 0; }
+    }
+
+    //ConcreteImplementation
+    public class OneManatCoupon : ICoupon
+    {
+        public int CouponValue { get => 1; }
+    }
+
+    //ConcreteImplementation
+    public class TwoManatCoupon : ICoupon
+    {
+        public int CouponValue { get => 2; }
+    }
 }
--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.Title = "Bridge";
+﻿using Bridge;
+
+Console.Title = "Bridge";
+
+var noCoupon = new NoCoupon();
+var oneManatCoupon = new OneManatCoupon();
+var twoManatCoupon = new TwoManatCoupon();
+
+
+var meatBasedMenu = new MeatbasedMenu(noCoupon);
+Console.WriteLine($"Meat based menu, no coupon: {meatBasedMenu.CalculatePrice()} manat.");
+
+var meatBasedMenu1 = new MeatbasedMenu(oneManatCoupon);
+Console.WriteLine($"Meat based menu, one manat coupon: {meatBasedMenu1.CalculatePrice()} manat.");
+
+var meatBasedMenu2 = new MeatbasedMenu(twoManatCoupon);
+Console.WriteLine($"Meat based menu, two manat coupon: {meatBasedMenu2.CalculatePrice()} manat.");
+
+
+var vegeterianMenu = new VegettarianMenu(noCoupon);
+Console.WriteLine($"Vegeterian menu, no coupon: {vegeterianMenu.CalculatePrice()} manat.");
+
+var vegeterianMenu1 = new VegettarianMenu(oneManatCoupon);
+Console.WriteLine($"Vegeterian menu, one manat coupon: {vegeterianMenu1.CalculatePrice()} manat.");
+
+var vegeterianMenu2 = new VegettarianMenu(twoManatCoupon);
+Console.WriteLine($"Vegeterian menu, two manat coupon: {vegeterianMenu2.CalculatePrice()} manat.");
