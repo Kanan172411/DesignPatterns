7197e412d2d8c90f32b9c8123c83aaaa0a483e0e
Kanan172411 <kanangarayev02@gmail.com>
2022-10-18 17:24:51 +0400

    add template method pattern

--- TemplateMethod/Implementation.cs
+++ TemplateMethod/Implementation.cs
@@ -1,3 +1,57 @@
 ﻿namespace TemplateMethod
 { 
+    //Abstract class
+    public abstract class MailParser
+    {
+        public virtual void FindServer()
+        {
+            Console.WriteLine("Finding server ...");
+        }
+
+        public abstract void AuthenticateToServer();
+
+        public string ParseHtmlMailBody(string identifier)
+        {
+            Console.WriteLine("Parsing Html mail body ...");
+            return $"This is the body of mail with id {identifier}";
+        }
+
+        //Template Method
+        public string ParseMailBody(string identifier)
+        {
+            Console.WriteLine("Parsing mail body (in template method) ...");
+            FindServer();
+            AuthenticateToServer();
+            return ParseHtmlMailBody(identifier);
+        }
+    }
+
+    public class ExchangeMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Exchange");
+        }
+    }
+
+    public class ApacheMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Apache");
+        }
+    }
+
+    public class EudoraMailparser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Eudora");
+        }
+
+        public override void FindServer()
+        {
+            Console.WriteLine("Finding Eudora server through a custom algorithm...");
+        }
+    }
 }
--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Title = "Template Method";
+﻿using TemplateMethod;
+
+Console.Title = "Template Method";
+
+ExchangeMailParser exchangeMailParser = new();
+Console.WriteLine(exchangeMailParser.ParseMailBody("7efee005-3964-4914-acd0-bf14ff3cff13"));
+
+Console.WriteLine("======================================================================");
+
+ApacheMailParser apacheMailParser = new();
+Console.WriteLine(apacheMailParser.ParseMailBody("56dc54bd-0146-4409-9014-8ec658e90834"));
+
+Console.WriteLine("======================================================================");
+
+EudoraMailparser eudoraMailParser = new();
+Console.WriteLine(eudoraMailParser.ParseMailBody("a6bc9dbc-da9a-4c16-845b-8515fd0317de"));
