128c2933e81e2675e4899a7ccc7224c952fd2820
Kanan172411 <kanangarayev02@gmail.com>
2022-10-18 15:44:04 +0400

    add flyWeight pattern

--- Flyweight/Implementation.cs
+++ Flyweight/Implementation.cs
@@ -1,3 +1,91 @@
 ﻿namespace Flyweight
 {
+    //FlyWeight
+    public interface ICharacter
+    {
+        void Draw(string fontFamily, int fontSize);
+    }
+
+    //Concrete FlyWeight
+    public class CharacterA : ICharacter
+    {
+        private char _actualCharacter = 'a';
+        private string _fontFamily = string.Empty;
+        private int _fontSize;
+        public void Draw(string fontFamily, int fontSize)
+        {
+            _fontFamily = fontFamily;
+            _fontSize = fontSize;
+            Console.WriteLine($"Drawing {_actualCharacter}, {_fontFamily} {_fontSize}");
+        }
+    }
+
+    //Concrete FlyWeight
+    public class CharacterB : ICharacter
+    {
+        private char _actualCharacter = 'b';
+        private string _fontFamily = string.Empty;
+        private int _fontSize;
+        public void Draw(string fontFamily, int fontSize)
+        {
+            _fontFamily = fontFamily;
+            _fontSize = fontSize;
+            Console.WriteLine($"Drawing {_actualCharacter}, {_fontFamily} {_fontSize}");
+        }
+    }
+
+    //FlyWeight Factory
+    public class CharacterFactory
+    {
+        private readonly Dictionary<char, ICharacter> _characters = new();
+
+        public ICharacter? GetCharacter(char characterIdentifier)
+        {
+            if (_characters.ContainsKey(characterIdentifier))
+            {
+                Console.WriteLine("Character reuse");
+                return _characters[characterIdentifier];
+            }
+
+            Console.WriteLine("Character construction");
+            switch (characterIdentifier)
+            {
+                case 'a':
+                    _characters[characterIdentifier] = new CharacterA();
+                    return _characters[characterIdentifier];
+                case 'b':
+                    _characters[characterIdentifier] = new CharacterB();
+                    return _characters[characterIdentifier];
+            }
+
+            return null;
+        }
+
+        public ICharacter CreateParagraph(List<ICharacter> characters, int location)
+        {
+            return new Paragraph(location, characters);
+        }
+    }
+
+    //Unshared Concrete FlyWeight
+    public class Paragraph : ICharacter
+    {
+        private int _location;
+        private List<ICharacter> _characters = new();
+
+        public Paragraph(int location, List<ICharacter> characters)
+        {
+            _location = location;
+            _characters = characters;
+        }
+
+        public void Draw(string fontFamily, int fontSize)
+        {
+            Console.WriteLine($"Drawing in paragraph at location {_location}");
+            foreach (var item in _characters)
+            {
+                item.Draw(fontFamily, fontSize);
+            }
+        }
+    }
 }
--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.Title = "Flyweight";
+﻿using Flyweight;
+
+Console.Title = "Flyweight";
+
+var characters = "aaabbbabbab";
+
+var characterFactory = new CharacterFactory();
+
+var characterObject = characterFactory.GetCharacter(characters[0]);
+characterObject?.Draw("Arial", 12);
+
+characterObject = characterFactory.GetCharacter(characters[1]);
+characterObject?.Draw("Times New Roman", 14);
+
+characterObject = characterFactory.GetCharacter(characters[2]);
+characterObject?.Draw("Serif", 10);
+
+characterObject = characterFactory.GetCharacter(characters[3]);
+characterObject?.Draw("Sans-Serif", 15);
+
+characterObject = characterFactory.GetCharacter(characters[4]);
+characterObject?.Draw("Monaco", 11);
+
+characterObject = characterFactory.GetCharacter(characters[5]);
+characterObject?.Draw("Calibri", 18);
+
+var paragraph = characterFactory.CreateParagraph(new List<ICharacter>() { characterObject }, 1);
+paragraph.Draw("Helvetica", 20);
+
+
