e4fdb4de4145fa854cb0536bf13b2c6bf1eca756
Kanan172411 <kanangarayev02@gmail.com>
2022-10-18 14:44:53 +0400

    add proxy pattern

--- Proxy/Implementation.cs
+++ Proxy/Implementation.cs
@@ -1,3 +1,86 @@
 ﻿namespace Proxy
 {
+    //Subject
+    public interface IDocument
+    {
+        void DisplayDocument();
+    }
+    //RealSubject
+    public class Document : IDocument
+    {
+        public string? Title { get; private set; }
+        public string? Content { get; private set; }
+        public int AuthorId { get; private set; }
+        public DateTimeOffset LastAccessed { get; private set; }
+        private string _fileName;
+
+        public Document(string fileName)
+        {
+            _fileName = fileName;
+            LoadDocument(fileName);
+        }
+
+        private void LoadDocument(string fileName)
+        {
+            Console.WriteLine("Executing expensive action: loading a file from disk");
+
+            Thread.Sleep(5000);
+
+            Title = "An expensive document";
+            Content = "Lots of content";
+            AuthorId = 1;   
+            LastAccessed = DateTimeOffset.Now;
+        }
+
+        public void DisplayDocument()
+        {
+            Console.WriteLine($"Title: {Title}, Content: {Content}");
+        }
+    }
+
+    //Proxy
+    public class DocumentProxy : IDocument
+    {
+        private Lazy<Document> _document;
+        private string _fileName;
+
+        public DocumentProxy(string fileName)
+        {
+            _fileName = fileName;
+            _document = new Lazy<Document>(() => new Document(_fileName));
+        }
+        public void DisplayDocument()
+        {
+            _document.Value.DisplayDocument();
+        }
+    }
+
+    //Proxy
+    public class ProtectedDocumentProxy : IDocument 
+    {
+        private string _fileName;
+        private string _userRole;
+        private DocumentProxy _documentProxy;
+
+        public ProtectedDocumentProxy(string fileName, string userRole)
+        {
+            _fileName = fileName;
+            _userRole = userRole;
+            _documentProxy = new DocumentProxy(_fileName);
+        }
+
+        public void DisplayDocument()
+        {
+            Console.WriteLine($"Entering display document in {nameof(ProtectedDocumentProxy)}");
+
+            if(_userRole != "Viewer")
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            _documentProxy.DisplayDocument();
+            Console.WriteLine($"Exiting display document in {nameof(ProtectedDocumentProxy)}");
+
+        }
+    }
 }
--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.Title = "Proxy";
+﻿using Proxy;
+Console.Title = "Proxy";
+
+//without proxy
+Console.WriteLine("Constructing Document");
+var myDocument = new Document("index.js");
+Console.WriteLine("Constructed");
+myDocument.DisplayDocument();
+
+Console.WriteLine("========================================================");
+
+//with proxy
+Console.WriteLine("Constructing Document proxy");
+var myDocumentProxy = new DocumentProxy("main.js");
+Console.WriteLine("Document Proxy Constructed");
+myDocumentProxy.DisplayDocument();
+
+Console.WriteLine("========================================================");
+
+//with chanied proxy
+Console.WriteLine("Constructing Protected Document Proxy");
+var myProtectedDocumentProxy = new ProtectedDocumentProxy("main.js", "Viewer");
+Console.WriteLine("Protected Document Proxy Constructed");
+myProtectedDocumentProxy.DisplayDocument();
+
+Console.WriteLine("========================================================");
+
+//with chained proxy no access
+Console.WriteLine("Constructing Protected Document Proxy");
+myProtectedDocumentProxy = new ProtectedDocumentProxy("main.js", "AnotherRole");
+Console.WriteLine("Protected Document Proxy Constructed");
+myProtectedDocumentProxy.DisplayDocument();
