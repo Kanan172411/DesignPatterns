48b0df04029f6238f561e25159e061436b7df482
Kanan Garazada <kanangarayev02@gmail.com>
2022-10-19 13:51:02 +0400

    add strategy pattern

--- Strategy/Implementation.cs
+++ Strategy/Implementation.cs
@@ -1,3 +1,59 @@
 ﻿namespace Strategy
 {
+    //Strategy
+    public interface IExportService
+    {
+        void Export(Order order);
+    }
+
+    //ConcreteStratege
+    public class JsonExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to Json");
+        }
+    }
+
+    //ConcreteStrategy
+    public class XMlExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to XML");
+        }
+    }
+
+    //ConcreteStrategy
+    public class CSVExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to CSV");
+        }
+    }
+
+    //Context
+    public class Order
+    {
+        public string Customer { get; set; }
+        public int Amount { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public IExportService? ExportService { get; set; }
+
+        public Order(string customer, int amount, string name)
+        {
+            Customer = customer;
+            Amount = amount;
+            Name = name;
+            ExportService = new CSVExportService();
+        }
+
+        public void Export()
+        {
+            ExportService?.Export(this);
+        }
+    }
 }
--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.Title = "Strategy";
+﻿using Strategy;
+
+Console.Title = "Strategy";
+
+Order order = new("Kanan", 5, "windows license");
+order.Export();
+
+order.ExportService = new XMlExportService();
+order.Export();
+
+order.ExportService = new JsonExportService();
+order.Export();
