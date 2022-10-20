8dad0e8bc7e808a5bbe11499d9b53dae8f3b35a4
Kanan Garazada <kanangarayev02@gmail.com>
2022-10-20 12:06:26 +0400

    add command pattern

--- Command/Implementation.cs
+++ Command/Implementation.cs
@@ -1,3 +1,160 @@
 ﻿namespace Command
-{
+{ 
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Employee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public List<Employee> Employees = new();
+        public Manager(int id, string name) : base(id, name)
+        {
+        }
+    }
+
+    //Receiver(interface)
+    public interface IEmployeeManagerRepository
+    {
+        void Add(int managerId, Employee employee);
+        void Remove(int managerId, Employee employee);
+        bool HasEmployee(int managerId, int employeeId);
+        void WriteDataStore();
+    }
+
+    //Receiver(implementation)
+    public class EmployeeManagerRepository : IEmployeeManagerRepository
+    {
+        private List<Manager> _managers = new()
+        {
+            new Manager (1, "Keith"),
+            new Manager (2, "Cersei"),
+            new Manager (3, "Arya")
+        };
+
+        public void Add(int managerId, Employee employee)
+        {
+            _managers.First(m => m.Id == managerId).Employees.Add(employee);
+        }
+
+        public bool HasEmployee(int managerId, int employeeId)
+        {
+            return _managers.First(m => m.Id == managerId).Employees.Any(e => e.Id == employeeId);
+        }
+
+        public void Remove(int managerId, Employee employee)
+        {
+            _managers.First(x => x.Id == managerId).Employees.Remove(employee);
+        }
+
+        public void WriteDataStore()
+        {
+            foreach (var manager in _managers)
+            {
+                Console.WriteLine($"Manager: {manager.Id}, {manager.Name}");
+                if (manager.Employees.Any())
+                {
+                    foreach (var employee in manager.Employees)
+                    {
+                        Console.WriteLine($"\t Employee: {employee.Id}, {employee.Name}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"\tNo employees.");
+                }
+            }
+        }
+    }
+
+    //Command
+    public interface ICommand
+    {
+        void Execute();
+        bool CanExecute();
+        void Undo();
+    }
+
+    //ConcreteCommand
+    public class AddEmployeeToManagerList : ICommand
+    {
+        private readonly IEmployeeManagerRepository _employeeManagerRepository;
+        private readonly int _managerId;
+        private readonly Employee? _employee;
+
+        public AddEmployeeToManagerList(IEmployeeManagerRepository employeeManagerRepository, int managerId, Employee? employee)
+        {
+            _employeeManagerRepository = employeeManagerRepository;
+            _managerId = managerId;
+            _employee = employee;
+        }
+
+        public bool CanExecute()
+        {
+            if (_employee == null)
+            {
+                return false;
+            }
+            if(_employeeManagerRepository.HasEmployee(_managerId, _employee.Id))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Execute()
+        {
+            if (_employee == null)
+            {
+                return;
+            }
+            _employeeManagerRepository.Add(_managerId, _employee);
+        }
+
+        public void Undo()
+        {
+            if(_employee == null)
+            {
+                return;
+            }
+            _employeeManagerRepository.Remove(_managerId, _employee);
+        }
+    }
+
+    //Invoker
+    public class CommandManager
+    {
+        private readonly Stack<ICommand> _commands = new();
+        public void Invoke(ICommand command)
+        {
+            if (command.CanExecute())
+            {
+                command.Execute();
+                _commands.Push(command);
+            }
+        }
+
+        public void Undo()
+        {
+            if (_commands.Any())
+            {
+                _commands.Pop()?.Undo();
+            }
+        }
+
+        public void UndoAll()
+        {
+            while (_commands.Any())
+            {
+                _commands.Pop()?.Undo();
+            }
+        }
+    }
 } 
--- Command/Program.cs
+++ Command/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.Title = "Command";
+﻿using Command;
+
+Console.Title = "Command";
+
+CommandManager commandManager = new();
+EmployeeManagerRepository repository = new ();
+
+commandManager.Invoke(
+    new AddEmployeeToManagerList(
+        repository, 1, new Employee(111, "Brandon")));
+repository.WriteDataStore();
+
+commandManager.Undo();
+repository.WriteDataStore();
+
+commandManager.Invoke(
+    new AddEmployeeToManagerList(
+        repository, 1, new Employee(222, "Stark")));
+repository.WriteDataStore();
+
+commandManager.Invoke(
+    new AddEmployeeToManagerList(
+        repository, 2, new Employee(333, "Theon")));
+repository.WriteDataStore();
+
+commandManager.UndoAll();
+repository.WriteDataStore();
