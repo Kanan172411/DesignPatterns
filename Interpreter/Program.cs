d86211f2e012ce6f1e93f37bb1cc800100cc6a0d
Kanan Garazada <kanangarayev02@gmail.com>
2022-10-25 18:03:51 +0400

    add interpreter pattern

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.Title = "Interpreter";
+﻿using Interpreter;
+
+Console.Title = "Interpreter";
+
+var expressions = new List<RomanExpression>
+{
+    new RomanHundredExpression(),
+    new RomanTenExpression(),
+    new RomanOneExpression()
+};
+
+var context = new RomanContext(5);
+foreach(var item in expressions)
+{
+    item.Interpret(context);
+}
+
+Console.WriteLine($"Translating Arabic numerals to Roman numerals: 5 = {context.Output}");
+Console.WriteLine("===================================================================");
+
+
+context = new RomanContext(81);
+foreach(var item in expressions)
+{
+    item.Interpret(context);
+}
+
+Console.WriteLine($"Translating Arabic numerals to Roman numerals: 81= {context.Output}");
+Console.WriteLine("===================================================================");
+
+
+context = new RomanContext(733);
+foreach(var item in expressions)
+{
+    item.Interpret(context);
+}
+
+Console.WriteLine($"Translating Arabic numerals to Roman numerals: 733 = {context.Output}");
