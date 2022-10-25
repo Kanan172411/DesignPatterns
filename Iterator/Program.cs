e0f2ae94430f456bbfc03039dce423efecb724d6
Kanan Garazada <kanangarayev02@gmail.com>
2022-10-25 15:47:53 +0400

    add iterator pattern

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Title = "Iterator";
+﻿using Iterator;
+
+Console.Title = "Iterator";
+
+PeopleCollection people = new();
+
+people.Add(new Person("Brandon Stark", "Winterfell"));
+people.Add(new Person("Tywin Lannister", "Casterly Rock"));
+people.Add(new Person("Robert Baratheon", "Hornhill"));
+people.Add(new Person("Margery Tyrell", "HighGarden"));
+
+var peopleIterator = people.CreateIterator();
+
+for (Person person = peopleIterator.First(); !peopleIterator.IsDone; person = peopleIterator.Next())
+{
+    Console.WriteLine(person?.Name);
+}
