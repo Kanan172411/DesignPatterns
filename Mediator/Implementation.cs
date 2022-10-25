b3e126fc841b397f3b798aa3275caaa2c3f80233
Kanan Garazada <kanangarayev02@gmail.com>
2022-10-25 09:52:05 +0400

    add mediator pattern

--- Mediator/Implementation.cs
+++ Mediator/Implementation.cs
@@ -1,4 +1,112 @@
 ﻿namespace Mediator
 {
+    //Mediator
+    public interface IChatRoom
+    {
+        void Register(TeamMember teamMember);
+        void Send(string from, string message);
+        void Send(string from, string to, string message);
+        void SendTo<T>(string from, string message) where T : TeamMember;
+    }
+
+    public abstract class TeamMember
+    {
+        private IChatRoom? _chatRoom;
+        public string Name { get; set; }
+
+        public TeamMember(string name)
+        {
+            Name = name;
+        }
+
+        internal void SetChatRoom(IChatRoom chatRoom)
+        {
+            _chatRoom = chatRoom;
+        }
+
+        public void Send(string message)
+        {
+            _chatRoom?.Send(Name, message);
+        }
+
+        public void SendTo<T>(string message) where T : TeamMember
+        {
+            _chatRoom?.SendTo<T>(Name, message);
+        }
+
+        public void Send(string to, string message)
+        {
+            _chatRoom?.Send(Name,to, message);
+        }
+
+        public virtual void Receive(string from, string message)
+        {
+            Console.WriteLine($"Message {from} to {Name}: {message}");
+        }
+    }
+
+    //ConcreteColleague
+    public class Lawyer : TeamMember
+    {
+        public Lawyer(string name) : base(name)
+        {
+        }
+
+        public override void Receive(string from, string message)
+        {
+            Console.WriteLine($"{nameof(Lawyer)} {Name} received.");
+            base.Receive(from, message);   
+        }
+    }
+
+    //ConcreteColleague
+    public class AcountManager : TeamMember
+    {
+        public AcountManager(string name) : base(name)
+        {
+        }
+
+        public override void Receive(string from, string message)
+        {
+            Console.WriteLine($"{nameof(AcountManager)} {Name} received.");
+            base.Receive(from, message);   
+        }
+    }
+
+    //ConcreteMediator
+    public class TeamChatRoom : IChatRoom
+    {
+        private readonly Dictionary<string, TeamMember> teamMembers = new();
+        public void Register(TeamMember teamMember)
+        {
+            teamMember.SetChatRoom(this);
+            if (!teamMembers.ContainsKey(teamMember.Name))
+            {
+                teamMembers.Add(teamMember.Name, teamMember);
+            }
+        }
+
+        public void Send(string from, string message)
+        {
+            foreach (var item in teamMembers.Values)
+            {
+                item.Receive(from, message);
+            }
+        }
+
+        public void Send(string from, string to, string message)
+        {
+            var teamMember = teamMembers[to];
+            teamMember?.Receive(from, message);
+        }
+
+        public void SendTo<T>(string from, string message) where T : TeamMember
+        {
+            foreach (var item in teamMembers.Values.OfType<T>())
+            {
+                item.Receive(from, message);
+            }
+        }
+    }
 }
  
--- Mediator/Program.cs
+++ Mediator/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.Title = "Mediator";
+﻿using Mediator;
+
+Console.Title = "Mediator";
+
+TeamChatRoom teamChatRoom = new();
+
+var bran = new Lawyer("Bran");
+var sansa = new Lawyer("Sansa");
+var snow = new AcountManager("Snow");
+var rob = new AcountManager("Rob");
+var rickon = new AcountManager("Rickon");
+
+teamChatRoom.Register(bran);
+teamChatRoom.Register(sansa);
+teamChatRoom.Register(snow);
+teamChatRoom.Register(rob);
+teamChatRoom.Register(rickon);
+
+rob.Send("Some message to everyone");
+snow.Send("Some answer to everyone");
+snow.Send("Rob","Some message to rob");
+rob.SendTo<Lawyer>("Some Message to lawyers");
