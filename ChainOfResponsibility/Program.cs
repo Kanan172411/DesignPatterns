d662990c9266ebb6df1b49fac561b5d96bcce437
Kanan Garazada <kanangarayev02@gmail.com>
2022-10-25 10:52:12 +0400

    add chainOfResponsibility

--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.Title = "Chain of Responsibility";
+﻿using ChainOfResponsibility;
+using System.ComponentModel.DataAnnotations;
+
+Console.Title = "Chain of Responsibility";
+
+var validDocument = new Document("How to avoid C# Development", DateTimeOffset.UtcNow, true, true);
+var invalidDocument = new Document("How to avoid C# Development", DateTimeOffset.UtcNow.AddDays(-60), true, true);
+
+var documentHandlerChain = new DocumentTitleHandler();
+documentHandlerChain
+    .SetSuccessor(new DocumentTitleHandler())
+    .SetSuccessor(new DocumentApprovedByLitigationHandler())
+    .SetSuccessor(new DocumentApprovedByManagerHandler())
+    .SetSuccessor(new DocumentLastModifiedHandler());
+
+try
+{
+    documentHandlerChain.Handle(validDocument);
+    Console.WriteLine("Valid document is valid");
+    documentHandlerChain.Handle(invalidDocument);
+    Console.WriteLine("Invalid document is valid");
+}
+catch (ValidationException ex)
+{
+    Console.WriteLine(ex.Message);
+}
